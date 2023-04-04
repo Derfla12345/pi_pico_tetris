0034f3ebd3234c631c15e0963b50f3931715338a
Alfred <129817104+Derfla12345@users.noreply.github.com>
2023-04-04 03:05:26 -0500

    one could call this complete tetris
    added rotation, it doesn't clip through walls or blocks
    added soft and hard dropping
    i only have 4 buttons, so one is now a modifier and one is the drop button
    modifier + left = rotate left
    modifier + drop = hard drop
    all 4 buttons = power off

--- main.py
+++ main.py
@@ -25,9 +25,10 @@ class Config(object):
         self.gameWidth = self.displayHeight - (self.wallWidth * 2)
         self.gameHeight = self.displayWidth
         # speed is the length of time.sleep() in the game's loop
-        self.speed = 0.025
-        # there is a variable tick that increments by 1 every loop until tick % tickrate == 0, at which point the game updates
-        self.tickrate = 2
+        self.speed = 0.001
+        # there is a variable tick that increments by 1 every loop until tick % tickrate == 0, at which point the game updates, TICKRATE is const while tickrate is modified
+        self.TICKRATE = 10
+        self.tickrate = self.TICKRATE
         # holdDelay is the amount of ticks between when you begin to hold a button and when it begins to repeat itsself
         self.holdDelay = 5
         # pieceTypes are the types of pieces possible
@@ -68,7 +69,7 @@ class Block(object):
         self.y = y
 
 class Tetrimino(object):
-    def __init__(self, pieceType, oled, config):
+    def __init__(self, pieceType, oled, config, playArea):
         self.root = Block(4, 1)
         self.b1 = Block()
         self.b2 = Block()
@@ -77,8 +78,8 @@ class Tetrimino(object):
         self.active = True
         self.rotationState = 0
         self.pieceType = pieceType
-        self._update(oled, config)
-    def _update(self, oled, config):
+        self._update(oled, config, playArea)
+    def _update(self, oled, config, playArea):
         if self.rotationState == 0:
             if self.pieceType == 'I':
                 if self.b1.y == self.root.y + 1:
@@ -205,10 +206,11 @@ class Tetrimino(object):
                 self.b1.update(self.root.x - 1, self.root.y + 1)
                 self.b2.update(self.root.x - 1, self.root.y)
                 self.b3.update(self.root.x, self.root.y - 1)
-        self._draw(oled, config)
-    def _draw(self, oled, config):
+        self.draw(oled, config, playArea)
+    def draw(self, oled, config, playArea):
         oled.fill(0)
         drawBorders(oled)
+        playArea.draw(oled, config)
         for block in self.blocks:
             block.draw(oled, config)
     def _place(self, oled, playArea):
@@ -217,34 +219,86 @@ class Tetrimino(object):
         for block in self.blocks:
             playArea.update(block)
         self.active = False
+    def rotationCheck(self, config):
+        self._rotationCheckLeft(config)
+        self._rotationCheckRight(config)
+        self._rotationCheckDown(config)
+    def _rotationCheckRight(self, config):
+        offset = 0
+        for block in self.blocks:
+            offset = min(offset, config.width - 1 - block.x)
+        for block in self.blocks:
+            block.x += offset
+    def _rotationCheckLeft(self, config):
+        offset = 0
+        for block in self.blocks:
+            offset = max(offset, -block.x)
+        for block in self.blocks:
+            block.x += offset
+    def _rotationCheckDown(self, config):
+        offset = 0
+        for block in self.blocks:
+            offset = min(offset, config.height - 2 - block.y)
+        for block in self.blocks:
+            block.y += offset
     def fall(self, oled, config, playArea):
         if ((not playArea.array[self.root.y + 1][self.root.x]) and (not playArea.array[self.b1.y + 1][self.b1.x]) and (not playArea.array[self.b2.y + 1][self.b2.x]) and (not playArea.array[self.b3.y + 1][self.b3.x])):
             for block in self.blocks:
                 block.y += 1
-            self._draw(oled, config)
+            self.draw(oled, config, playArea)
         else:
             self._place(oled, playArea)
     def moveLeft(self, playArea):
+        collision = False
         blockX = [block.x for block in self.blocks]
         minBlock = self.blocks[blockX.index(min(blockX))]
-        if minBlock.x > 0 and not playArea.array[minBlock.y][minBlock.x - 1]:
+        if minBlock.x > 0:
             for block in self.blocks:
-                block.x -= 1
+                if playArea.array[block.y][block.x - 1]:
+                    collision = True
+            if not collision:
+                for block in self.blocks:
+                    block.x -= 1
     def moveRight(self, config, playArea):
+        collision = False
         blockX = [block.x for block in self.blocks]
         maxBlock = self.blocks[blockX.index(max(blockX))]
-        if maxBlock.x < config.width - 1 and not playArea.array[maxBlock.y][maxBlock.x + 1]:
+        if maxBlock.x < config.width - 1:
             for block in self.blocks:
-                block.x += 1
+                if playArea.array[block.y][block.x + 1]:
+                    collision = True
+            if not collision:
+                for block in self.blocks:
+                    block.x += 1
+    def _checkUndo(self, playArea, undoBlocks):
+        collision = False
+        for block in self.blocks:
+            if playArea.array[block.y][block.x]:
+                print("whoops!")
+                collision = True
+        if collision:
+            for i in range(len(self.blocks)):
+                self.blocks[i].x = undoBlocks[i].x
+                self.blocks[i].y = undoBlocks[i].y
     def rotateRight(self, oled, config, playArea):
+        undoBlocks = [Block(self.root.x, self.root.y), Block(self.b1.x, self.b1.y), Block(self.b2.x, self.b2.y), Block(self.b3.x, self.b3.y)]
         self.rotationState = (self.rotationState + 1) % 4
-        self._update(oled, config)
+        self._update(oled, config, playArea)
+        self.rotationCheck(config)
+        self._checkUndo(playArea, undoBlocks)
     def rotateLeft(self, oled, config, playArea):
         if self.rotationState == 0:
             self.rotationState = 3
         else:
             self.rotationState -= 1
-        self._update(oled, config)
+        self._update(oled, config, playArea)
+        self.rotationCheck(config)
+    def hardDrop(self, oled, config, playArea):
+        while ((not playArea.array[self.root.y + 1][self.root.x]) and (not playArea.array[self.b1.y + 1][self.b1.x]) and (not playArea.array[self.b2.y + 1][self.b2.x]) and (not playArea.array[self.b3.y + 1][self.b3.x])):
+            for block in self.blocks:
+                block.y += 1
+        self._place(oled, playArea)
+        self.draw(oled, config, playArea)
         
 ##########################################################################################################################
 # FUNCTIONS:
@@ -280,45 +334,78 @@ def drawBorders(oled):
     oled.fill_rect(0, 63, 128, 1, 1)
     oled.fill_rect(123, 0, 5, 64, 1)
 
-# 'L' is left, 'R' is right, 'O' is off, 0 is none 
-def checkButtons(offButton, leftButton, rightButton, rotateButton):
+# 'L' is left, 'R' is right, 'D' is (soft) drop, 'M' is modifier (ML is rotate left, MR is rotate right, MD is hard drop)
+def checkButtons(dropButton, leftButton, rightButton, modifyButton):
     buttonPressed = 0
-    if offButton.value() != 1:
-        buttonPressed = 'O'
-    elif leftButton.value() != 1:
-        buttonPressed = 'L'
-    elif rightButton.value() != 1:
-        buttonPressed = 'R'
-    elif rotateButton.value() != 1:
-        buttonPressed = 'T'
+    if modifyButton.value() != 1 or dropButton.value() != 1 or leftButton.value() != 1 or rightButton.value() != 1:
+        buttonPressed = ''
+        if modifyButton.value() != 1:
+            buttonPressed += 'M'
+        if dropButton.value() != 1:
+            buttonPressed += 'D'
+        if leftButton.value() != 1:
+            buttonPressed += 'L'
+        if rightButton.value() != 1:
+            buttonPressed += 'R'
     return buttonPressed
 
 # determines if the button is pressed and performs the relevant actions if so
 def evaluateButton(oled, config, playArea, t1, buttonPressed, tick, holdTick, holdingButton):
-    if buttonPressed:
-        if not holdTick:
-            holdTick = tick
-            moveBlock(oled, config, playArea, t1, buttonPressed)
-        elif ((tick - holdTick) % config.holdDelay == 0) or (holdingButton == True):
-            holdingButton = True
-            moveBlock(oled, config, playArea, t1, buttonPressed)
+    if buttonPressed and buttonPressed != 'M':
+        holdTick, holdingButton = holdCheck(oled, config, playArea, t1, buttonPressed, tick, holdTick, holdingButton)
     else:
+        normalDrop(config)
         holdTick = 0
         holdingButton = False
     return(holdTick, holdingButton)
 
-# moves the block either left or right
+# checks if buttons are held and sends inputs
+def holdCheck(oled, config, playArea, t1, buttonPressed, tick, holdTick, holdingButton):
+    if not holdTick:
+        holdTick = 1
+        moveBlock(oled, config, playArea, t1, buttonPressed)
+    elif holdTick < config.holdDelay:
+        holdTick += 1
+    elif (holdTick >= config.holdDelay) or (holdingButton == True):
+        holdingButton = True
+        if buttonPressed != 'ML' and buttonPressed != 'MR':
+            moveBlock(oled, config, playArea, t1, buttonPressed)
+    return(holdTick, holdingButton)
+
+# moves the block either left or right, or rotates it
 def moveBlock(oled, config, playArea, t1, buttonPressed):
     if buttonPressed == 'L':
         t1.moveLeft(playArea)
     elif buttonPressed == 'R':
         t1.moveRight(config, playArea)
-    elif buttonPressed == 'T':
+    elif buttonPressed == 'ML':
+        t1.rotateLeft(oled, config, playArea)
+    elif buttonPressed == 'MR':
         t1.rotateRight(oled, config, playArea)
+    elif buttonPressed == 'D':
+        softDrop(config)
+    elif buttonPressed == 'MD':
+        hardDrop(oled, config, playArea, t1)
+
+# speeds up the rate at which tetriminos fall
+def softDrop(config):
+    config.tickrate = 2
+
+# instantly drops a tetrimino
+def hardDrop(oled, config, playArea, t1):
+    t1.hardDrop(oled, config, playArea)
+
+# returns tetrimino fall rate to normal
+def normalDrop(config):
+    config.tickrate = config.TICKRATE
 
 # determine if the game has been lost (called at the creation of every new tetrimino)
-def checkIfLost(config, playArea, block):
-    return playArea.array[1][block.x]
+def checkIfLost(config, playArea, t1):
+    lost = False
+    for block in t1.blocks:
+        if playArea.array[1][block.x]:
+            lost = True
+    return lost
 
 # check for clearable lines
 def checkClear(playArea):
@@ -378,10 +465,10 @@ def main():
     # DEFINITIONS:
     potentiometer = ADC(26)
     screenOff = False
-    offButton = Pin(0, Pin.IN, Pin.PULL_UP)
-    leftButton = Pin(1, Pin.IN, Pin.PULL_UP)
-    rightButton = Pin(2, Pin.IN, Pin.PULL_UP)
-    rotateButton = Pin(3, Pin.IN, Pin.PULL_UP)
+    dropButton = Pin(0, Pin.IN, Pin.PULL_UP) # k2
+    leftButton = Pin(1, Pin.IN, Pin.PULL_UP) # k0
+    rightButton = Pin(2, Pin.IN, Pin.PULL_UP) # k1
+    modifyButton = Pin(3, Pin.IN, Pin.PULL_UP) # k3
     config = Config()
     conversion_factor, i2c, oled = readyDisplay(config)
     playArea = Area(config.height, config.width)
@@ -392,33 +479,34 @@ def main():
     canFall = True
     hasLost = False
     buttonPressed = 0
+    linesCleared = 0
     sevenBagger = [False for i in range(len(config.pieceTypes))]
     sevenBagger, pieceType = randomizer(sevenBagger, config)
-    t1 = Tetrimino(pieceType, oled, config)
+    t1 = Tetrimino(pieceType, oled, config, playArea)
     # NOT DEFINITIONS:
-    
-    # the game's 20 tall, 10 wide
+    # the game's 20 tall, 10 wide (indices are different and weird)
     # blocks are drawn within the bounds y = 3 to y = 57, x = 0 to x = 118
     
-    while buttonPressed != 'O':
+    while buttonPressed != 'MDLR':
         # draw borders
         drawBorders(oled)
         
         # poll buttons
-        buttonPressed = checkButtons(offButton, leftButton, rightButton, rotateButton)
+        buttonPressed = checkButtons(dropButton, leftButton, rightButton, modifyButton) #stopped here
 
         # falling tetrimino
         if t1.active == True:
             holdTick, holdingButton = evaluateButton(oled, config, playArea, t1, buttonPressed, tick, holdTick, holdingButton)
-            if tick % config.tickrate == 0:
+            if tick >= config.tickrate:
                 t1.fall(oled, config, playArea)
                 playArea.draw(oled, config)
+                tick = 0
         else:
             sevenBagger, pieceType = randomizer(sevenBagger, config)
-            t1 = Tetrimino(pieceType, oled, config)
-            hasLost = checkIfLost(config, playArea, t1.root)
+            t1 = Tetrimino(pieceType, oled, config, playArea)
+            hasLost = checkIfLost(config, playArea, t1)
             b1.active = not hasLost
-            checkClear(playArea)
+            linesCleared += checkClear(playArea)
         
         if hasLost:
             print("you lose")
@@ -428,6 +516,7 @@ def main():
         tick = tick + 1
         
         # display
+        t1.draw(oled, config, playArea)
         oled.show()
         
         # polling/refresh rate
@@ -435,13 +524,14 @@ def main():
 
     # off button was pressed, while loop was ended:
     oled.poweroff()
+    print(linesCleared)
 
 ##########################################################################################################################
 # call main:
 main()
 
 """
-next: rotation! (oh jeez)
+add score, next, or ramping up speed
 """
   
 
